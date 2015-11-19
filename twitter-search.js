8de0067a5d168d32d2379372e788f95f1c9c3203
Morton <github@technopolis.de>
2015-11-19 20:19:51 +0100

    added loading of topics

--- twitter-search.js
+++ twitter-search.js
@@ -26,7 +26,7 @@ function loadTweets(q, max_id, since_id) {
     });
 }
 function filterSearchResult(data) {
-    return new Promise(function (res, rej) {
+    return new Promise(function (res) {
         var result = {};
 
         var parsedMaxId = /.*?max_id=(.*?)&.*?/.exec(data.search_metadata.next_results);
@@ -35,6 +35,26 @@ function filterSearchResult(data) {
             max_id: (parsedMaxId && parsedMaxId.length > 1 ? parsedMaxId[1] : undefined),
             since_id: (parsedSinceId && parsedSinceId.length > 1 ? parsedSinceId[1] : undefined)
         };
+        result.topics = data.statuses.filter(function (v) {
+            // ignore RTs
+            if (v.text.substr(0, 3) === "RT ") {
+                return false;
+            }
+
+            // only tweets by pixel_dailies can contain topics
+            return v.user.screen_name.toLowerCase() == 'pixel_dailies';
+        })/*.map(function (v) {
+            return {
+                text: v.text,
+                creation_date: v.created_at,
+                hashtags: (v.entities.hashtags ? v.entities.hashtags.map(function (v) { return v.text }) : []),
+                mentions: (v.entities.user_mentions ? v.entities.user_mentions.map(function (v) { return v.screen_name }) : []),
+                medias: (v.entities.media ? v.entities.media.map(function (v) { return { url: v.media_url, type: v.type } }) : []),
+                id: v.id,
+                user_id: v.user.screen_name,
+                user_name: v.user.name
+            };
+        })*/;
         result.tweets = data.statuses.filter(function (v) {
             // ignore RTs
             if (v.text.substr(0, 3) === "RT ") {
@@ -52,6 +72,36 @@ function filterSearchResult(data) {
                 id: v.id,
                 user_id: v.user.screen_name,
                 user_name: v.user.name
+            };
+        });
+
+        res(result);
+    });
+}
+function filterTopics(data) {
+    return new Promise(function (res) {
+        var result = {};
+
+        var parsedMaxId = /.*?max_id=(.*?)&.*?/.exec(data.search_metadata.next_results);
+        var parsedSinceId = /.*?since_id=(.*?)&.*?/.exec(data.search_metadata.refresh_url);
+        result.meta = {
+            max_id: (parsedMaxId && parsedMaxId.length > 1 ? parsedMaxId[1] : undefined),
+            since_id: (parsedSinceId && parsedSinceId.length > 1 ? parsedSinceId[1] : undefined)
+        };
+        result.topics = data.statuses.filter(function (v) {
+            // ignore RTs
+            if (v.text.substr(0, 3) === "RT ") {
+                return false;
+            }
+
+            // only tweets by pixel_dailies can contain topics
+            return v.user.screen_name.toLowerCase() == 'pixel_dailies';
+        }).map(function (v) {
+            return {
+                text: v.text,
+                creation_date: v.created_at,
+                hashtags: (v.entities.hashtags ? v.entities.hashtags.map(function (v) { return v.text }) : []),
+                id: v.id
             };
         });
 
@@ -78,14 +128,17 @@ if (fileExists(filename)) {
     allTweets = {
         meta: {
             max_id: Number.MAX_VALUE,
-            since_id: undefined
+            since_id: undefined,
+            topics_max_id: Number.MAX_VALUE,
+            topics_since_id: undefined
         },
-        tweets: []
+        tweets: [],
+        topics: []
     };
 }
 
 // define correct tweet-range
-var max_id, since_id;
+var max_id, since_id, topics_max_id, topics_since_id;
 if (allTweets.meta.max_id) {
     max_id = allTweets.meta.max_id;
     since_id = undefined;
@@ -93,10 +146,32 @@ if (allTweets.meta.max_id) {
     max_id = undefined;
     since_id = allTweets.meta.since_id;
 }
+if (allTweets.meta.topics_max_id) {
+    topics_max_id = allTweets.meta.topics_max_id;
+    topics_since_id = undefined;
+} else {
+    topics_max_id = undefined;
+    topics_since_id = allTweets.meta.topics_since_id;
+}
 
 // process tweets
 loadTweets('#pixel_dailies @Pixel_Dailies ', max_id, since_id)
     .then(filterSearchResult)
+    .then((tweets) => new Promise((res, rej) => {
+        loadTweets('from:Pixel_Dailies ', topics_max_id, topics_since_id)
+            .then(filterTopics)
+            .then(function (topics) {
+                console.log("Got", topics.topics.length, "new", (topics_max_id?"tail":"head"), "topics.");
+
+                tweets.meta.topics_max_id = topics.meta.max_id;
+                tweets.meta.topics_since_id = topics.meta.since_id;
+                tweets.topics = topics.topics;
+                res(tweets);
+            })
+            .catch(function (err) {
+                rej(err);
+            });
+    }))
     .then(function (tweets) {
         console.log("Got", tweets.tweets.length, "new", (max_id?"tail":"head"), "tweets.");
 
@@ -107,9 +182,16 @@ loadTweets('#pixel_dailies @Pixel_Dailies ', max_id, since_id)
         if (tweets.meta.since_id > allTweets.meta.since_id) {
             allTweets.meta.since_id = tweets.meta.since_id;
         }
+        if (tweets.meta.topics_max_id < allTweets.meta.topics_max_id) {
+            allTweets.meta.topics_max_id = tweets.meta.topics_max_id;
+        }
+        if (tweets.meta.topics_since_id > allTweets.meta.topics_since_id) {
+            allTweets.meta.topics_since_id = tweets.meta.topics_since_id;
+        }
         allTweets.tweets = allTweets.tweets.concat(tweets.tweets);
+        allTweets.topics = allTweets.topics.concat(tweets.topics);
 
-        console.log("Collection has", allTweets.tweets.length, " at all.");
+        console.log("Collection has", allTweets.tweets.length, "tweets and", allTweets.topics.length, "topics at all.");
 
         // write to file
         jsonfile.writeFileSync(filename, allTweets);
