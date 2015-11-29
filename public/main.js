dd816a022a82b5fcb06a3683ede99ba8f3b0c95b
Morton <github@technopolis.de>
2015-11-29 19:04:10 +0100

    change webapp folder structure

--- public/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Pixel_dailies from 'pixel_dailies';
-
-
-ReactDOM.render(<Pixel_dailies />, document.getElementById('ReactContainer'));
